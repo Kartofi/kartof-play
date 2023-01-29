b9c15f94adc92229cf842366bde2f6044d06856a
Kartofi <anatoli7707@gmail.com>
2023-01-29 13:55:07 +0200

    node mongo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fetch = (...args) =>
 const express = require("express");
 const app = express();
 const port = 3000;
-const { MongoClient, Timestamp } = require("mongodb");
+//const { MongoClient, Timestamp } = require("mongodb");
 require("dotenv").config();
 
 //const url = process.env.mongodb;
